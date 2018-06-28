ecfc291631f23d8781baa2245441547eaaca9133
monilpshah <monil31199@gmail.com>
2018-06-28 14:44:50 +0530

    Final

--- app/src/main/java/com/example/pc/scoreboard/MainActivity.java
+++ app/src/main/java/com/example/pc/scoreboard/MainActivity.java
@@ -7,9 +7,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int teama=0;
-    int teamb=0;
-    int wicketA=0, wicketB=0;
+    int teama = 0, teamb = 0, wicketA = 0, wicketB = 0;
 
 
     @Override
@@ -19,72 +17,65 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void add1(View view) {
-        teama+=1;
+        teama += 1;
         displayA(teama);
     }
-
 
     public void add2(View view) {
-        teama+=2;
+        teama += 2;
         displayA(teama);
     }
 
-
-
     public void add3(View view) {
-        teama+=3;
+        teama += 3;
         displayA(teama);
     }
 
     public void wicketA(View view) {
-        wicketA+=1;
+        wicketA += 1;
         displaywicketA(wicketA);
     }
 
-    public  void  displaywicketA(int number){
+    public void displaywicketA(int number) {
         TextView textView = (TextView) findViewById(R.id.wicket);
         textView.setText("" + number);
     }
 
-
     public void badd1(View view) {
-        teamb+=1;
+        teamb += 1;
         displayB(teamb);
     }
-
 
     public void badd2(View view) {
-        teamb+=2;
+        teamb += 2;
         displayB(teamb);
     }
 
-
     public void badd3(View view) {
-        teamb+=3;
+        teamb += 3;
         displayB(teamb);
     }
 
     public void wicketB(View view) {
-        wicketB+=1;
+        wicketB += 1;
         displaywicketB(wicketB);
     }
 
-    public  void  displaywicketB(int number){
+    public void displaywicketB(int number) {
         TextView textView = (TextView) findViewById(R.id.wicketB);
         textView.setText("" + number);
     }
 
-    public void reset(View view){
-        teama=0;
-        teamb=0;
-        wicketA=0;
-        wicketB=0;
+    public void reset(View view) {
+        teama = 0;
+        teamb = 0;
+        wicketA = 0;
+        wicketB = 0;
         TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText("0");
 
         TextView textView1 = (TextView) findViewById(R.id.textView1);
         textView1.setText("0");
-
 
 
         TextView txtView1 = (TextView) findViewById(R.id.wicket);
@@ -106,6 +97,4 @@ public class MainActivity extends AppCompatActivity {
         TextView textView1 = (TextView) findViewById(R.id.textView1);
         textView1.setText("" + number);
     }
-
-
 }
